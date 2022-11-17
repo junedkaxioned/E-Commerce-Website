d0d2b9d596e86d70c554fcf885e27ff8d96e17f1
junedkaxioned <junedk@axioned.com>
2022-11-17 17:42:29 +0530

    Mobile Nav, Sticky navbar Function Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-/* Author: Zishan Ansari */
+
+var html = document.querySelector('html');
+var header = document.querySelector("header");
+var hamBurger = document.querySelector('.hamburger');
+var navbar = document.querySelector('.navbar');
+
+
+// Events On Windows Start
+window.addEventListener('scroll', function () {
+  stickyNav();
+});
+// Events On Windows End
+
+// Sticky Navigation Function Start 
+function stickyNav() {
+  if (window.scrollY > header.offsetHeight + 8) {
+    header.classList.add('active')
+  } else {
+    header.classList.remove('active')
+  }
+}
+
+// Mobile Navbar Function Start 
+hamBurger.addEventListener('click', function () {
+  html.classList.toggle('stop-scroll')
+  hamBurger.classList.toggle('active')
+  navbar.classList.toggle('active')
+})
+// Mobile Navbar Function End 
